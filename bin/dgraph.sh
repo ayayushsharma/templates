f021f70a5ace567f9b2e327a0ba0ec159bf11e80
Ayush Sharma <64801786+ayayushsharma@users.noreply.github.com>
2023-12-30 01:06:38 +0530

    non graph debug can handle multiple test cases

--- bin/dgraph.sh
+++ bin/dgraph.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-echo 'Executing for debugging graph problems'
+# this script assumes that you are using Competitive Programming helper
+#
+if [ ! -d ~/projects/cp/.cph ]; then
+  echo "./.cph does not exist. Use CPH for this script"
+  exit
+fi
 
-g++ -std=c++20 -DLOCAL -DDEBUG_GRAPH $1 -o /tmp/graph.out
+spinner() {
+  PID=$!
+  i=1
+  sp="/-\|"
+  echo -n ' '
+  while [ -d /proc/$PID ]; do
+    printf "\b${sp:i++%${#sp}:1}"
+  done
+}
+# compiling
+g++ -std=c++20 -DLOCAL -DDEBUG_GRAPH $1 -o /tmp/graph.out &
+spinner
 
 if [ ! -f /tmp/graph.out ]; then
-  echo "Compilation Failed!"
+  echo -en "\rCompilation Failed!\n"
 else
-  /tmp/graph.out <~/projects/cp/test.graph
+  echo -en "\rCompilation Done\n"
+
+  mkdir -p ~/projects/cp/test.graph/
+
+  # read each item in the JSON array to an item in the Bash array
+  array="$(cat ./.cph/"."$1* | jq -c -S '.tests[]')"
+  readarray -t my_array <<<"$array"
+
+  # iterate through the Bash array
+  for item in "${my_array[@]}"; do
+    input="$(jq --raw-output '.input' <<<"$item")"
+    id="$(jq --raw-output '.id' <<<"$item")"
+    echo "$input" >./test.graph/$1.$id.test
+  done
+
+  for file in ~/projects/cp/test.graph/$1.*; do
+    [ -f "$file" ] && echo "Processing $file" && /tmp/graph.out <"$file"
+  done
+
   rm /tmp/graph.out
   rm /tmp/graph.svg
   rm /tmp/graph.dot
