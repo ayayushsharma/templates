69e188a1aa09e9298fc7a965fcc6e8f5188edac0
Ayush Sharma <64801786+ayayushsharma@users.noreply.github.com>
2024-09-28 02:16:00 +0530

    Changing config to suit the need
    - Refactoring code mostly
    - adding support for nvim comptitest

--- debug/debug.hpp
+++ debug/debug.hpp
@@ -1,15 +1,15 @@
 #ifndef CP_DEBUG_H
+#define CP_DEBUG_H
 
 #if defined(DEBUG_GRAPH)
-#include "graph/graph.h"
+#include "graph/graph.hpp"
 #else
 #define graph(...)
 #endif
 
-#define CP_DEBUG_H
 #define echo(...)                                      \
   debugging::__debug_print(#__VA_ARGS__, __VA_ARGS__); \
-  cout << endl;
+  cout << "\n";
 #define TESTCASE cout << "case " << i << ": " << endl;
 #define fast_io ios_base::sync_with_stdio(true), std::cin.tie(0);
 #define tab "    "
@@ -18,109 +18,109 @@
 namespace debugging {
 template <typename T>
 void _print(T c) {
-  cout << c;
+  std::cout << c;
 }
 
 template <typename T, typename V>
-void _print(pair<T, V> c) {
-  cout << "{";
+void _print(std::pair<T, V> c) {
+  std::cout << "{";
   debugging::_print(c.first);
-  cout << ",";
+  std::cout << ",";
   debugging::_print(c.second);
-  cout << "}";
+  std::cout << "}";
 }
 
 template <typename T>
-void _print(priority_queue<T> c) {
-  cout << '{';
+void _print(std::priority_queue<T> c) {
+  std::cout << '{';
   if (c.empty()) {
-    cout << "}";
+    std::cout << "}";
     return;
   }
   while (c.size() > 1) {
     debugging::_print(c.top());
     c.pop();
-    cout << ",";
+    std::cout << ",";
   }
   debugging::_print(c.top());
-  cout << "}";
+  std::cout << "}";
 }
 
 template <typename T>
-void _print(priority_queue<T, vector<T>, greater<T>> c) {
-  cout << '{';
+void _print(std::priority_queue<T, std::vector<T>, std::greater<T>> c) {
+  std::cout << '{';
   if (c.empty()) {
-    cout << "}";
+    std::cout << "}";
     return;
   }
   while (c.size() > 1) {
     debugging::_print(c.top());
     c.pop();
-    cout << ",";
+    std::cout << ",";
   }
   debugging::_print(c.top());
-  cout << "}";
+  std::cout << "}";
 }
 
 template <typename T>
-void _print(vector<T> c) {
+void _print(std::vector<T> c) {
   int f = 0;
-  cout << '{';
-  for (auto i : c) cout << (f++ ? "," : ""), debugging::_print(i);
-  cout << "}";
+  std::cout << '{';
+  for (auto i : c) std::cout << (f++ ? "," : ""), debugging::_print(i);
+  std::cout << "}";
 }
 
 template <typename T>
-void _print(vector<vector<T>> c) {
-  cout << "\n" smalltab "{\n" tab;
+void _print(std::vector<std::vector<T>> c) {
+  std::cout << "\n" smalltab "{\n" tab;
   int f = 0;
   for (auto &i : c) {
-    cout << (f++ ? ",\n" tab : ""), debugging::_print(i);
+    std::cout << (f++ ? ",\n" tab : ""), debugging::_print(i);
   }
-  cout << "\n" smalltab "}\n ";
+  std::cout << "\n" smalltab "}\n ";
 }
 
 template <typename T>
-void _print(set<T> c) {
+void _print(std::set<T> c) {
   int f = 0;
-  cout << '{';
-  for (auto &i : c) cout << (f++ ? ", " : ""), debugging::_print(i);
-  cout << "}";
+  std::cout << '{';
+  for (auto &i : c) std::cout << (f++ ? ", " : ""), debugging::_print(i);
+  std::cout << "}";
 }
 
 template <typename T>
-void _print(multiset<T> c) {
+void _print(std::multiset<T> c) {
   int f = 0;
-  cout << '{';
-  for (auto &i : c) cout << (f++ ? ", " : ""), debugging::_print(i);
-  cout << "}";
+  std::cout << '{';
+  for (auto &i : c) std::cout << (f++ ? ", " : ""), debugging::_print(i);
+  std::cout << "}";
 }
 
 template <typename T, typename V>
-void _print(map<T, V> c) {
-  cout << endl;
+void _print(std::map<T, V> c) {
+  std::cout << "\n";
   for (auto x : c) {
-    cout << smalltab;
+    std::cout << smalltab;
     debugging::_print(x.first);
-    cout << "  ";
+    std::cout << "  ";
     debugging::_print(x.second);
-    cout << endl;
+    std::cout << "\n";
   }
 }
 
 template <typename T>
-void __deb(istringstream &ss, T arg) {
-  string n;
+void __deb(std::istringstream &ss, T arg) {
+  std::string n;
   getline(ss, n, ',');
   const auto pos(n.find_first_not_of(" "));
   n.erase(0, pos);
-  cout << "[" << n << ": ";
+  std::cout << "[" << n << ": ";
   debugging::_print(arg);
-  cout << "] ";
+  std::cout << "] ";
 }
 template <typename... T>
 void __debug_print(const char *n, T &&...args) {
-  istringstream ss(n);
+  std::istringstream ss(n);
   (__deb(ss, args), ...);
 }
 }  // namespace debugging
--- debug/graph/graphUtils.hpp
+++ debug/graph/graphUtils.hpp
@@ -6,8 +6,10 @@
 
 #define CP_GRAPHUTILS_H
 
-const string GRAPH_DOT_FILE_LOCATION = "/tmp/graph.dot";
-const string GRAPH_SVG_FILE_LOCATION = "/tmp/graph.svg";
+#include <bits/stdc++.h>
+
+const std::string GRAPH_DOT_FILE_LOCATION = "/tmp/graph.dot";
+const std::string GRAPH_SVG_FILE_LOCATION = "/tmp/graph.svg";
 const int GRAPH_DEFAULT_HEIGHT = 600;
 const int GRAPH_DEFAULT_WIDTH = 600;
 
@@ -16,16 +18,16 @@ namespace graphUtils {
  * returns the vertices in the order a < b in a set of {a, b} on whatever output
  * you give to it
  */
-pair<int, int> distinctEdge(int a, int b) {
-  if (a > b) swap(a, b);
-  return make_pair(a, b);
+std::pair<int, int> distinctEdge(int a, int b) {
+  if (a > b) std::swap(a, b);
+  return std::make_pair(a, b);
 }
 
 /**
  * This function finds number of "new line" ('\n') in the string to approximate
  * the number of edges in the graph
  */
-int numberOfEdgesFromDotFile(const string &graphDotString) {
+int numberOfEdgesFromDotFile(const std::string &graphDotString) {
   int count = 0;
   for (size_t i = 0; i < graphDotString.length(); i++)
     if (graphDotString[i] == '\n') ++count;
@@ -35,7 +37,7 @@ int numberOfEdgesFromDotFile(const string &graphDotString) {
 /**
  *
  */
-void exec(string &command_str) {
+void exec(std::string &command_str) {
   const char *COMMAND = command_str.c_str();
   system(COMMAND);
 }
@@ -43,7 +45,7 @@ void exec(string &command_str) {
  * Write the supplied string to the configured dot file.
  * Also returns the string supplied
  */
-void dotFile(const string &graphDotString) {
+void dotFile(const std::string &graphDotString) {
   std::ofstream graphDotFile(GRAPH_DOT_FILE_LOCATION);
   graphDotFile << graphDotString;
   graphDotFile.close();
@@ -53,7 +55,7 @@ void dotFile(const string &graphDotString) {
  * Create a SVG from the dot file
  */
 void createGraphSVG() {
-  string COMMAND =
+  std::string COMMAND =
       "dot -Tsvg " + GRAPH_DOT_FILE_LOCATION + " -o " + GRAPH_SVG_FILE_LOCATION;
   graphUtils::exec(COMMAND);
 }
@@ -62,9 +64,10 @@ void createGraphSVG() {
  * Prints the svg in form of sixel in height * width
  */
 void createGraphSixel(int HEIGHT, int WIDTH) {
-  string COMMAND = "vv --preview-size=" + std::to_string(max(HEIGHT, WIDTH)) +
-                   " --geometry=" + std::to_string(HEIGHT) + "x" +
-                   std::to_string(WIDTH) + " " + GRAPH_SVG_FILE_LOCATION;
+  std::string COMMAND =
+      "vv --preview-size=" + std::to_string(max(HEIGHT, WIDTH)) +
+      " --geometry=" + std::to_string(HEIGHT) + "x" + std::to_string(WIDTH) +
+      " " + GRAPH_SVG_FILE_LOCATION;
   graphUtils::exec(COMMAND);
 }
 /**
@@ -72,15 +75,15 @@ void createGraphSixel(int HEIGHT, int WIDTH) {
  */
 void createGraphSixel(int number_of_edges) {
   int FACTOR = sqrt(number_of_edges);
-  int HEIGHT = min(1000, FACTOR * GRAPH_DEFAULT_HEIGHT / 2),
-      WIDTH = min(1800, FACTOR * GRAPH_DEFAULT_WIDTH / 2);
+  int HEIGHT = std::min(1000, FACTOR * GRAPH_DEFAULT_HEIGHT / 2),
+      WIDTH = std::min(1800, FACTOR * GRAPH_DEFAULT_WIDTH / 2);
   graphUtils::createGraphSixel(HEIGHT, WIDTH);
 }
 
 /**
  * Print the graph String in sixel format in console
  */
-void printSixelGraph(const string &graphString) {
+void printSixelGraph(const std::string &graphString) {
   int numberOfEdges = graphUtils::numberOfEdgesFromDotFile(graphString);
   graphUtils::dotFile(graphString);
   graphUtils::createGraphSVG();
