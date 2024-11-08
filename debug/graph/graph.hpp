4f7b12557684cbb80208fb9015c479508cf4cf55
Ayush Sharma <64801786+ayayushsharma@users.noreply.github.com>
2024-11-09 02:32:37 +0530

    formatting changes

--- debug/graph/graph.hpp
+++ debug/graph/graph.hpp
@@ -5,11 +5,11 @@
 #ifndef CP_GRAPH_H
 #define CP_GRAPH_H
 
-#include "graphUtils.hpp"
-
 #include <set>
 #include <string>
 #include <vector>
+
+#include "graphUtils.hpp"
 
 #define graph(x, y) graph::printGraph(x, y)
 
@@ -17,79 +17,79 @@ namespace graph {
 
 inline std::string undirectedGraphDotString(
     const std::vector<std::vector<int>> &graph) {
-  // to store distinct edges between to vertices
-  std::set<std::pair<int, int>> edge;
+    // to store distinct edges between to vertices
+    std::set<std::pair<int, int>> edge;
 
-  // creating edges
-  for (int parent = 0; parent < graph.size(); parent++) {
-    for (int child = 0; child < graph[parent].size(); child++) {
-      edge.insert(graphUtils::distinctEdge(parent, graph[parent][child]));
+    // creating edges
+    for (int parent = 0; parent < graph.size(); parent++) {
+        for (int child = 0; child < graph[parent].size(); child++) {
+            edge.insert(graphUtils::distinctEdge(parent, graph[parent][child]));
+        }
     }
-  }
 
-  // making a string to put into the graph.dot file
-  std::string dotString = "graph {\n";
-  for (auto vertices : edge) {
-    dotString += std::to_string(vertices.first) + " -- " +
-                 std::to_string(vertices.second) + ";\n";
-  }
-  dotString += "}";
+    // making a string to put into the graph.dot file
+    std::string dotString = "graph {\n";
+    for (auto vertices : edge) {
+        dotString += std::to_string(vertices.first) + " -- " +
+                     std::to_string(vertices.second) + ";\n";
+    }
+    dotString += "}";
 
-  // return the dot string
-  return dotString;
+    // return the dot string
+    return dotString;
 }
 
 inline std::string directedGraphDotString(
     const std::vector<std::vector<int>> &graph) {
-  // to store distinct edges between to vertices
-  std::set<std::pair<int, int>> edge;
+    // to store distinct edges between to vertices
+    std::set<std::pair<int, int>> edge;
 
-  // creating edges
-  for (int parent = 0; parent < graph.size(); parent++) {
-    for (int child = 0; child < graph[parent].size(); child++) {
-      edge.insert(std::make_pair(parent, graph[parent][child]));
+    // creating edges
+    for (int parent = 0; parent < graph.size(); parent++) {
+        for (int child = 0; child < graph[parent].size(); child++) {
+            edge.insert(std::make_pair(parent, graph[parent][child]));
+        }
     }
-  }
 
-  // making a string to put into the graph.dot file
-  std::string dotString = "digraph {\n";
-  for (auto vertices : edge) {
-    dotString += std::to_string(vertices.first) + " -> " +
-                 std::to_string(vertices.second) + ";\n";
-  }
-  dotString += "}";
+    // making a string to put into the graph.dot file
+    std::string dotString = "digraph {\n";
+    for (auto vertices : edge) {
+        dotString += std::to_string(vertices.first) + " -> " +
+                     std::to_string(vertices.second) + ";\n";
+    }
+    dotString += "}";
 
-  // return the dot string
-  return dotString;
+    // return the dot string
+    return dotString;
 }
 
 inline void printUndirectedGraph(const std::vector<std::vector<int>> &graph) {
-  std::string graphString = graph::undirectedGraphDotString(graph);
-  graphUtils::printSixelGraph(graphString);
+    std::string graphString = graph::undirectedGraphDotString(graph);
+    graphUtils::printSixelGraph(graphString);
 }
 
 inline void printDirectedGraph(const std::vector<std::vector<int>> &graph) {
-  std::string graphString = graph::directedGraphDotString(graph);
-  graphUtils::printSixelGraph(graphString);
+    std::string graphString = graph::directedGraphDotString(graph);
+    graphUtils::printSixelGraph(graphString);
 }
 
 /**
  * Defaults to print graph
  */
 inline void printGraph(const std::vector<std::vector<int>> &graph, char type) {
-  switch (type) {
-  case 'U':
-  case 'u':
-    graph::printUndirectedGraph(graph);
-    break;
-  case 'D':
-  case 'd':
-    graph::printDirectedGraph(graph);
-    break;
-  default:
-    graph::printUndirectedGraph(graph);
-    break;
-  }
+    switch (type) {
+    case 'U':
+    case 'u':
+        graph::printUndirectedGraph(graph);
+        break;
+    case 'D':
+    case 'd':
+        graph::printDirectedGraph(graph);
+        break;
+    default:
+        graph::printUndirectedGraph(graph);
+        break;
+    }
 }
 
 }  // namespace graph
--- debug/graph/graphUtils.hpp
+++ debug/graph/graphUtils.hpp
@@ -6,11 +6,10 @@
 
 #define CP_GRAPHUTILS_H
 
-#include <string>
-#include <fstream>
 #include <algorithm>
 #include <cmath>
-
+#include <fstream>
+#include <string>
 
 const std::string GRAPH_DOT_FILE_LOCATION = "/tmp/graph.dot";
 const std::string GRAPH_SVG_FILE_LOCATION = "/tmp/graph.svg";
@@ -23,8 +22,8 @@ namespace graphUtils {
  * you give to it
  */
 std::pair<int, int> distinctEdge(int a, int b) {
-  if (a > b) std::swap(a, b);
-  return std::make_pair(a, b);
+    if (a > b) std::swap(a, b);
+    return std::make_pair(a, b);
 }
 
 /**
@@ -32,66 +31,66 @@ std::pair<int, int> distinctEdge(int a, int b) {
  * the number of edges in the graph
  */
 int numberOfEdgesFromDotFile(const std::string &graphDotString) {
-  int count = 0;
-  for (size_t i = 0; i < graphDotString.length(); i++)
-    if (graphDotString[i] == '\n') ++count;
-  return count;
+    int count = 0;
+    for (size_t i = 0; i < graphDotString.length(); i++)
+        if (graphDotString[i] == '\n') ++count;
+    return count;
 }
 
 /**
  *
  */
 void exec(std::string &command_str) {
-  const char *COMMAND = command_str.c_str();
-  system(COMMAND);
+    const char *COMMAND = command_str.c_str();
+    system(COMMAND);
 }
 /**
  * Write the supplied string to the configured dot file.
  * Also returns the string supplied
  */
 void dotFile(const std::string &graphDotString) {
-  std::ofstream graphDotFile(GRAPH_DOT_FILE_LOCATION);
-  graphDotFile << graphDotString;
-  graphDotFile.close();
+    std::ofstream graphDotFile(GRAPH_DOT_FILE_LOCATION);
+    graphDotFile << graphDotString;
+    graphDotFile.close();
 }
 
 /**
  * Create a SVG from the dot file
  */
 void createGraphSVG() {
-  std::string COMMAND =
-      "dot -Tsvg " + GRAPH_DOT_FILE_LOCATION + " -o " + GRAPH_SVG_FILE_LOCATION;
-  graphUtils::exec(COMMAND);
+    std::string COMMAND = "dot -Tsvg " + GRAPH_DOT_FILE_LOCATION + " -o " +
+                          GRAPH_SVG_FILE_LOCATION;
+    graphUtils::exec(COMMAND);
 }
 
 /**
  * Prints the svg in form of sixel in height * width
  */
 void createGraphSixel(int HEIGHT, int WIDTH) {
-  std::string COMMAND =
-      "vv --preview-size=" + std::to_string(max(HEIGHT, WIDTH)) +
-      " --geometry=" + std::to_string(HEIGHT) + "x" + std::to_string(WIDTH) +
-      " " + GRAPH_SVG_FILE_LOCATION;
-  graphUtils::exec(COMMAND);
+    std::string COMMAND =
+        "vv --preview-size=" + std::to_string(max(HEIGHT, WIDTH)) +
+        " --geometry=" + std::to_string(HEIGHT) + "x" + std::to_string(WIDTH) +
+        " " + GRAPH_SVG_FILE_LOCATION;
+    graphUtils::exec(COMMAND);
 }
 /**
  * Prints the svg in form of sixel and resizes it automatically
  */
 void createGraphSixel(int number_of_edges) {
-  int FACTOR = std::sqrt(number_of_edges);
-  int HEIGHT = min(1000, FACTOR * GRAPH_DEFAULT_HEIGHT / 2),
-      WIDTH = min(1800, FACTOR * GRAPH_DEFAULT_WIDTH / 2);
-  graphUtils::createGraphSixel(HEIGHT, WIDTH);
+    int FACTOR = std::sqrt(number_of_edges);
+    int HEIGHT = min(1000, FACTOR * GRAPH_DEFAULT_HEIGHT / 2),
+        WIDTH = min(1800, FACTOR * GRAPH_DEFAULT_WIDTH / 2);
+    graphUtils::createGraphSixel(HEIGHT, WIDTH);
 }
 
 /**
  * Print the graph String in sixel format in console
  */
 void printSixelGraph(const std::string &graphString) {
-  int numberOfEdges = graphUtils::numberOfEdgesFromDotFile(graphString);
-  graphUtils::dotFile(graphString);
-  graphUtils::createGraphSVG();
-  graphUtils::createGraphSixel(numberOfEdges);
+    int numberOfEdges = graphUtils::numberOfEdgesFromDotFile(graphString);
+    graphUtils::dotFile(graphString);
+    graphUtils::createGraphSVG();
+    graphUtils::createGraphSixel(numberOfEdges);
 }
 }  // namespace graphUtils
 
